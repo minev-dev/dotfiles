8e849e1131e57bcc1317620fd3913ff6f62ba560
Vladimir Minev <minev.dev@gmail.com>
2026-02-09 19:33:58 -0800

    Load lualine on startup

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -46,6 +46,7 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    lazy = false,
     config = function()
       require("lualine").setup {
         sections = {
